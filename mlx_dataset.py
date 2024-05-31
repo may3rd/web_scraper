145e0fe937a0ada5082ed5510c10360d4951fba5
Maetee L <may3rd@gmail.com>
2024-05-31 14:46:23 +0700

    updated requirement

--- mlx_dataset.py
+++ mlx_dataset.py
@@ -23,36 +23,45 @@ def format_id(id_value):
     except ValueError:
         # If conversion fails, return the original value as string
         return str(id_value)
-
-input_file = 'results/test.json'
-output_file = 'results/train.jsonl'
-
-# load JSON file
-data = pd.read_json(input_file)
-
-# Apply the function to the columns
-data['ID'] = data['ID'].astype(str)
-data["ID"] = data['ID'].apply(format_id)
-data['Keywords'] = data['Keywords'].apply(convert_keywords)
-data['Solutions'] = data['Solutions'].apply(convert_solutions)
-
-ids = list(data["ID"])
-problems = list(data['Problem Statement'])
-solutions = list(data["Solutions"])
-keywords = list(data["Keywords"])
-
-def data_create(id:str,question:str,answer:str,keywords:str):
-    chat = {"messages": [
-  {"role": "user", "content": f"Instruct: Give me the solution of the following problem.\n.{question}"},
-  {"role": "assistant", "content": f"{answer}.\nID: {id}\nKeywords: {keywords}"},
+    
+# create message instruction from data
+def data_create(
+        id:str,question:str = '',
+        answer:str = '',
+        keywords:str = '',
+        instruction:str = 'Give me the solution of the following problem.'
+        ):
+    return {"messages": [
+  {"role": "user", "content": f"Instruct: {instruction}\n{question}"},
+  {"role": "assistant", "content": f"{answer}\nID: {id}\nKeywords: {keywords}"},
 ]}
-    return chat
 
-train_data = [data_create(id, question, answer, keyword) for id, question, answer, keyword in zip(ids, problems, solutions, keywords)]
+def main(input_file:str = 'results/test.json', output_file:str = 'results/train.jsonl'):
 
-# save dataset as jsonl format
-with open(output_file, "w") as fid:
-    for t in train_data:
-        json.dump(t, fid)
-        fid.write("\n")
+    # load JSON file
+    data = pd.read_json(input_file)
 
+    # Apply the function to the columns
+    data['ID'] = data['ID'].astype(str)
+    data["ID"] = data['ID'].apply(format_id)
+    data['Keywords'] = data['Keywords'].apply(convert_keywords)
+    data['Solutions'] = data['Solutions'].apply(convert_solutions)
+
+    # extract the id, problem statement, soluction, and keywords
+    ids = list(data["ID"])
+    problems = list(data['Problem Statement'])
+    solutions = list(data["Solutions"])
+    keywords = list(data["Keywords"])
+
+    # convert extracted to train dataset
+    train_data = [data_create(id, question, answer, keyword) for id, question, answer, keyword in zip(ids, problems, solutions, keywords)]
+
+    # save dataset as jsonl format
+    with open(output_file, "w") as fid:
+        for t in train_data:
+            json.dump(t, fid)
+            fid.write("\n")
+
+
+if __name__ == "__main__":
+    main('results/test.json', 'results/train.jsonl')
